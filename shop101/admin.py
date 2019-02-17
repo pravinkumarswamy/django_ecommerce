be9e7489823fd99cde8205d883b04b7e58959c04
pravinkumarswamy <pravin06.ps@gmail.com>
2019-02-17 15:19:47 +0530

    initial commit

---
+++ shop101/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Product,Cart
+
+admin.site.register(Product)
+admin.site.register(Cart)
+
+
+# Register your models here.
---
+++ shop101/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render,HttpResponse
+from shop101.models import Product
+
+
+# Create your views here.
+def test(request):
+    return HttpResponse('hello world')
+
+def products_list(request):
+    products=Product.objects.all()
+    print(products)
+    return render(request,'products_list.html',context={'product_list':products})
+
+def show_product(request,id):
+    p_id=id
+    product=Product.objects.get(id=p_id)
+    return render(request,'show_product.html',context={'product':product})
+
+
